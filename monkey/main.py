0ca8a4c8c7e75446a6fd283f88189fa18f1535d7
Azu Nwaokobia <azdonald@yahoo.co.uk>
2017-10-18 12:49:49 -0400

    added prompt to raw input

--- monkey/main.py
+++ monkey/main.py
@@ -3,10 +3,10 @@ from .lexer import Lexer
 
 print '>>> Welcome to the Monkey programming language'
 
-s = raw_input()
+s = raw_input('>>  ')
 while s != 'END':
     tok = Lexer(s)
     while tok.readPosition < len(s):
         tk = tok.nextToken()
         print 'Token type  is {} while literal is {} '.format(tk.tokenType, tk.literal)     
-    s = raw_input()
+    s = raw_input('>>  ')
